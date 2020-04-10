8d9400bcec6cc5c5aa8f44e3c6a1bc5f8641d885
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-10 09:49:29 -0700

    adds additional business logic and initial user interface logic for form data

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,55 @@ Pizza.prototype.addToppings = function(topping) {
 }
 
 Pizza.prototype.sizePrice = function() {
-  if (this.size === small) {
+  if (this.size === "small") {
     return 7.00;
-  } else if (this.size === medium) {
+  } else if (this.size === "medium") {
     return 9.00;
-  } else if (this.size === large) {
+  } else if (this.size === "large") {
     return 11.00;
   }
 }
 
-Pizza.prototype.totalPrice = function() {
-  totalPrice += this.sizePrice();
-  this.toppings.forEach(function(topping){
-    totalPrice += toppingPrice(topping);
+Pizza.prototype.toppingPrice = function() {
+  totalToppingPrice = 0.00;
+  this.toppings.forEach(function(topping) {
+    if (topping === "pepperoni" || topping === "mushrooms") {
+      totalToppingPrice += 1.00;
+      console.log("adding topping prices" + totalToppingPrice);
+    } else {
+      totalToppingPrice += 2.00;
+      console.log("adding topping prices" + totalToppingPrice);
+    }
+    return totalToppingPrice;
   })
 }
 
-function Topping() {
-  this.pepperoni = pepperoni,
-  this.extraCheese = extraCheese,
-  this.mushrooms = mushrooms
-}
-
-Topping.prototype.toppingPrice = function(topping) {
-  if (topping === pepperoni || topping === mushrooms) {
-    return 1.00;
-  } else {
-    return 2.00;
-  }
+Pizza.prototype.totalPrice = function() {
+  var totalPrice = 0
+  totalPrice += this.sizePrice();
+  console.log("add size price" + totalPrice);
+  totalPrice += this.toppingPrice();
+  console.log("add total topping price" + totalPrice);
+  return totalPrice;
 }
 
 
 // User Interface Logic -------------------------------------
+$(document).ready(function() {
+  $("form#pizza-input").submit(function(event) {
+    event.preventDefault();
+
+    
+    var size = $("#size").val();
+    var pizza = new Pizza(size);
+
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var topping = $(this).val();
+      pizza.addToppings(topping);
+    })
+
+    var pizzaPrice = pizza.totalPrice();
+    console.log(pizzaPrice);
+
+  })
+})
